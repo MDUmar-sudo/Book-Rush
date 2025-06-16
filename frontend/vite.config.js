e9c0f90d8dda5bd2cc395c76409c934ce1f58127
MdUmar <risingsheikh@gmail.com>
2025-06-16 21:50:00 +0530

    update on 16/06/2025 v4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
+import path from 'path';
 
-// https://vite.dev/config/
+dotenv.config({ path: path.resolve('./frontend/.env') }); // Loading environment variables from .env
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // backend root
+        /* 
+        target: 'http://localhost:3000', // backend server for local development
+        */
+        target: import.meta.env.VITE_API_URL, // backend root
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
